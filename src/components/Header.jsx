38edfcd5915da83d1545c53248d2a3ab891b3453
Sajid Shahid <79515742+muhammadsajid3674@users.noreply.github.com>
2023-09-20 17:55:27 +0500

    Homepage UI Created

---
+++ src/components/Header.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+   return (
+      <nav className='navbar row justify-content-center sticky-top'>
+         <div className='container'>
+            <div className='col-3 p-0'>
+               <div className='navbar-brand'>
+                  <Image
+                     src='/images/bookit_logo.png'
+                     alt=''
+                     style={{ cursor: "pointer" }}
+                     width={145}
+                     height={33}
+                     priority
+                  />
+               </div>
+            </div>
+
+            <div className='col-3 mt-3 mt-md-0 text-center'>
+               <a className='btn btn-danger px-4 text-white login-header-btn float-right'>
+                  Login
+               </a>
+            </div>
+         </div>
+      </nav>
+   );
+};
+
+export default Header;
